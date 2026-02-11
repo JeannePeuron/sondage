d70827c8e535340a4c80411be4a5a95afb54c363
JeannePeuron <jeanne.peuron@gmail.com>
2026-02-11 16:02:50 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,61 @@
+// Tu reçois des réponses à un sondage sous forme de tableau :
+
+// ["yes", "no", "yes", "maybe", "YES", " no "]
+// Écris surveyStats(answers) qui retourne :
+
+// {
+//   yes: 2,
+//   no: 2,
+//   maybe: 1,
+//   invalid: 1
+// }
+// Règles :
+
+// réponses valides : "yes", "no", "maybe"
+
+// ignore la casse + espaces
+
+// tout le reste va dans invalid
+
+// Je dois crée un nouveau tableau donc je l'initialise
+// Dans ce tableau j'ai un objet avec 4 clés. Dois-je les initialiser ?
+// Parcourir le tableau pour lire les réponses
+// Quand je parcoure le tableau avec forEach, quand je croise une valeur qui est ok j'augmente le compteur initialisé
+
+function surveyStats(answers) {
+  // Il faut d'abord parcourir le tableau pour enlever les espaces avec trim
+  // Dans l'exemple le YES maj n'est pas pris en compte mais je vais quand même faire en sorte
+  // qu'il le soit avec to lowercar !
+
+  for (let i = 0; i < answers.length; i++) {
+    answers[i] = answers[i].trim().toLowerCase();
+  }
+
+  let newArray = [
+    {
+      yes: 0,
+      no: 0,
+      maybe: 0,
+      invalid: 0,
+    },
+  ];
+
+  answers.forEach((element) => {
+    if (element != "yes" && element != "no" && element != "maybe") {
+      newArray[0].invalid += 1;
+    } else if (element === "yes") {
+      newArray[0].yes += 1;
+    }
+    if (element === "no") {
+      newArray[0].no += 1;
+    }
+    if (element === "maybe") {
+      newArray[0].maybe += 1;
+    }
+  });
+  return newArray;
+}
+
+const array = ["yes", "no", "yes", "maybe", "YES", " no ", "yas"];
+
+console.log(surveyStats(array));
